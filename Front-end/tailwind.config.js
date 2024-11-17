10aecad155be3d242ae72be0626e03bb1a3a8894
Yacine4coding <bensidahmedyacine.off@gmail.com>
2024-11-17 19:11:14 +0100

    delete yacine '

---
+++ Front-end/tailwind.config.js
@@ -0,0 +1,11 @@
+// /** @type {import('tailwindcss').Config} */
+
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./app/**/*.{js,ts,jsx,tsx}", // For the /app directory (Next.js 13+)
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
