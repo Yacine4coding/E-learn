d31c1256941e0fa5a214fdf85e5913b83062ad34
benabdallahsamir <samirbenabdallah4002@gmail.com>
2024-12-31 13:23:04 +0100

    hundle favorite courses and dashboard state

--- Front-end/redux/dashboard.js
+++ Front-end/redux/dashboard.js
@@ -3,22 +3,50 @@ const { createSlice } = require("@reduxjs/toolkit");
 const dashboard = createSlice({
   initialState: {
     courses: [],
+    favoriteCourse: [],
+    wishlistCourse: [],
   },
   name: "dashboard",
   reducers: {
     initCourses: (state, { payload: courses }) => {
-      state.courses = courses
-      // state = { courses };
+      state.courses = courses;
+    },
+    initFavCourses: (state, { payload: courses }) => {
+      state.favoriteCourse = courses;
+    },
+    initWishlistCourses: (state, { payload: courses }) => {
+      state.wishlistCourse = courses;
     },
     toggleFavorite: (state, { payload }) => {
-      const { courses } = state;
-      const result = courses.map((ele) => {
-        if (ele.courseId === payload) ele.isFavorite = !ele.isFavorite;
-        return ele;
-      });
-      state.courses = result
+      try {
+        const { favoriteCourse } = state;
+        console.log("favoriteCourse")
+        console.log(state.favoriteCourse)
+        let removed = false;
+        const result = favoriteCourse.filter((ele) => {
+          if (ele.id === payload.id) {
+            removed = true;
+            console.log("enter")
+          }
+          return ele.id !== payload.id;
+        });
+        if (removed) {
+          state.favoriteCourse = result;
+        } else {
+          state.favoriteCourse = [...favoriteCourse, payload];
+          console.log("state.favoriteCourse");
+          console.log(state.favoriteCourse);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 });
-export const { toggleFavorite, initCourses } = dashboard.actions;
+export const {
+  toggleFavorite,
+  initCourses,
+  initFavCourses,
+  initWishlistCourses,
+} = dashboard.actions;
 export default dashboard.reducer;
